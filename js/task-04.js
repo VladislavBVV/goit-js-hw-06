723c1ac1be48685488d6203216dd647a89850c6d
Vladislav <Bliznyuk.vladislav@gmail.com>
2022-09-07 19:41:53 +0300

    04

---
+++ js/task-04.js
@@ -0,0 +1,46 @@
+const counter = {
+    value: 0,
+    increment() {
+        console.log('increment ->', this);
+        this.value += 1;
+    },
+    decrement() {
+        console.log('decrement -> this', this);
+        this.value -= 1;
+    },
+};
+
+
+
+const decrBtn = document.querySelector('.decr');
+const incrBtn = document.querySelector('.incr');
+const valueEl = document.querySelector('#value');
+
+
+console.log(decrBtn);
+console.log(incrBtn);
+console.log(valueEl);
+
+// decrBtn.textContent = '-1';
+
+decrBtn.addEventListener('click', function () {
+    console.log('Кликнули на декремент');
+
+    counter.decrement();
+    console.log(counter);
+    valueEl.textContent = counter.value;
+
+
+
+
+});
+
+incrBtn.addEventListener('click', function () {
+    console.log("Кликнули на инкремент");
+    
+     counter.increment();
+    console.log(counter);
+     valueEl.textContent = counter.value;
+
+});
+
